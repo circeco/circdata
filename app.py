e1fd042e51ab7fb42a3e9b335ca2c38b4df08a97
Piero <grilli.piero@circeco.org>
2020-04-17 16:47:18 +0000

    template inheritance & materialize set up

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'circdata'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://Circeco:Planet20@cluster0-bsyeh.mongodb.net/circdata?retryWrites=true&w=majority')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_circular_initiative')
+def get_circular_initiative():
+    return render_template("tasks.html", circular_initiative=mongo.db.circular_initiative.find())
 
 
 if __name__ == '__main__':
