a66aab22933be3a9f5af5a24b2f5333b55ded5ed
Piero <grilli.piero@circeco.org>
2020-04-28 18:35:56 +0000

    hide username and password of mongo

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'circdata'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://Circeco:Planet20@cluster0-bsyeh.mongodb.net/circdata?retryWrites=true&w=majority')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
